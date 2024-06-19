a60f2e9536af12b2ed24deacbd6aaf711f29b3da
sofiacostas <sofiacostas@riccarton.school.nz>
2024-06-20 09:50:51 +1200

    Changed the names of the views and removed code since it needs  reworking

--- players_stats.py
+++ players_stats.py
@@ -26,42 +26,11 @@ menu_choice =''
 while menu_choice != 'Z':
     menu_choice = input('Welcome to the Tottenham Hotspurs players database for the 23/24 season\n\n'
                         'Please type the letter for the information you want\n'
-                        'A: All information\n'
-                        'B: Top 8 goalscorers\n'
-                        'C: Most minutes played\n'
-                        'D: Players with at least 1 goal and 1 assist\n'
-                        'E: Centre-Forward players stats\n'
-                        'F: All non England players\n'
-                        'G: Games played and amount of yellow cards\n'
-                        'H: Players with under 2000 minutes played\n'
-                        'I: Players under 25 years old\n'
-                        'J: Top 5 players with the most assists\n'
-                        'K: Players with at least 25 games and 2500 mins played\n'
+    
                         'Z: Exit\n\n Type option here: ')
 
     menu_choice = menu_choice.upper()
-    if menu_choice == 'A':
-        print_query('All information')
-    elif menu_choice == 'B':
-        print_query('Top 8 goalscorers')
-    elif menu_choice == 'C':
-        print_query('Most minutes played')
-    elif menu_choice == 'D':
-        print_query('Players with at least 1 goal and 1 assist')
-    elif menu_choice == 'E':
-        print_query('Centre-Forward players stats')
-    elif menu_choice == 'F':
-        print_query('All non England players')
-    elif menu_choice == 'G':
-        print_query('Games played and amount of yellow cards')
-    elif menu_choice == 'H':
-        print_query('Players with under 2000 minutes played')
-    elif menu_choice == 'I':
-        print_query('Players under 25')
-    elif menu_choice == 'J':
-        print_query('Top 5 players with most assists ')
-    elif menu_choice == 'K':
-        print_query('Players with at least 25 games and 2500 mins played')
+
 
 
 
